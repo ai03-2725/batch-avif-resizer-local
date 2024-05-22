12088c2d2a2163ab7cc11b9d9d5b46280193323c
ai03 <admin@ai03.com>
2024-05-22 11:34:16 +0900

    Add thumbnail size generation for slider

--- image_autoresize.py
+++ image_autoresize.py
@@ -15,7 +15,8 @@ IMAGE_SIZES = [
   # ((3840, 2561), 1.5 * 1000 * 1000), # 4K -> 1.5MB (Only used for very hires gallery views where load times aren't crucial)
   ((2560, 2049), 500 * 1000), # 2560 -> 500KB (Heros and backgrounds)
   ((2048, 1025), 300 * 1000), # 2048 -> 300KB (Large gallery views)
-  ((1024, 0), 100 * 1000), # 1024 -> 100KB (Thumbnail-tier)
+  ((1024, 0), 100 * 1000), # 1024 -> 100KB (Medium views)
+  ((256, 0), 10 * 1000), # 256 -> 10KB (Thumbnail-tier)
 ]
 NO_RESIZE_MULTIPLIER = 0.4
 
